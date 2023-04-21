772155d5e9184449ae7c882954556b8fa6edb3f1
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-21 22:57:07 +0000

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Create a file
 file { '/tmp/school':
+  ensure  => present,
+  content => 'I lobe Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I lobe Puppet",
 }
