9b06a139cc6385bae26170fe5be431d7e95bd4eb
Mohammed Zakaria Kokoriquaye <70409308+MOHZAK93@users.noreply.github.com>
2023-07-23 18:12:42 +0000

    Delete 0x17-web_stack_debugging_3 directory
    Deferred from alx program

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-#Fix a faulty wordpress site
-
-exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-	/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
-}
